22539cb80381b3b4e42577883f0c9fb976367d65
ethan <knot1657@kettering.edu>
2023-02-15 17:57:22 -0500

    test

--- src/main/java/cs351/group5/App.java
+++ src/main/java/cs351/group5/App.java
@@ -1,15 +1,41 @@
 package cs351.group5;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
 
 /**
  * Hello world!
  *origin/Ethan-Test
+ *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+        int storeID = 2044; //Used to input store ID number
+        Table selectedTable = null;
+        DynamoDB dynamoDB = new DynamoDB(new AmazonDynamoDBClient(
+                new ProfileCredentialsProvider()));
+        TableCollection<ListTablesResult> tables = dynamoDB.listTables();
+        Iterator<Table> iterator = tables.iterator();
+        while (iterator.hasNext()) { //loops through all tables
+            Table tempTable = iterator.next();
+            System.out.println("Current table: " + tempTable.getTableName());
+            if(tempTable.getTableName().equalsIgnoreCase(selectedStore)) //if current table=wanted table
+            {
+                selectedTable = tempTable;
+                break;
+            }
+        }
+        System.out.println("Selected table: " + selectedTable.getTableName());
+        Item item = selectedTable.getItem("IDnum", 109);
         System.out.println( "Hello World!" );
-
     }
 }
