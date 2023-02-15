cfae566e0c6594407fde8ca206ad00276f34aaec
ethan <knot1657@kettering.edu>
2023-02-15 18:34:53 -0500

    Added getItemInfo() and newItem() method

--- src/main/java/cs351/group5/App.java
+++ src/main/java/cs351/group5/App.java
@@ -10,32 +10,55 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.dynamodbv2.model.ListTablesResult;
 
-/**
- * Hello world!
- *origin/Ethan-Test
- *
- */
 public class App
 {
     public static void main( String[] args )
     {
-        int storeID = 2044; //Used to input store ID number
+        //System.out.println(getItemInfo(2044, 1135)); //example code to pul item info based on storeid and itemid
+        //newItem(2044, 2001, "Cards", 1, false, "Funny Card", 5, 10); //example code to add new item to table
+    }
+
+    public static Table getTable(int storeID)
+    {
+        String tableName = "PartySuppliesStore" + storeID;
         Table selectedTable = null;
         DynamoDB dynamoDB = new DynamoDB(new AmazonDynamoDBClient(
                 new ProfileCredentialsProvider()));
-        TableCollection<ListTablesResult> tables = dynamoDB.listTables();
+        TableCollection<ListTablesResult> tables = dynamoDB.listTables(); //lists all tables
         Iterator<Table> iterator = tables.iterator();
         while (iterator.hasNext()) { //loops through all tables
             Table tempTable = iterator.next();
-            System.out.println("Current table: " + tempTable.getTableName());
-            if(tempTable.getTableName().equalsIgnoreCase(selectedStore)) //if current table=wanted table
+            if(tempTable.getTableName().equalsIgnoreCase(tableName)) //if current table=wanted table
             {
                 selectedTable = tempTable;
                 break;
             }
         }
-        System.out.println("Selected table: " + selectedTable.getTableName());
-        Item item = selectedTable.getItem("IDnum", 109);
-        System.out.println( "Hello World!" );
+        return selectedTable;
+    }
+
+    public static String getItemInfo(int storeID, int itemID)
+    {
+        Table selectedTable = getTable(storeID);
+        Item currentItem = selectedTable.getItem("ItemID", itemID); //looks up item
+        return currentItem.toJSONPretty(); //returns item info (json)
+    }
+
+    public static void newItem
+            (
+            int storeID, int itemID, String itemCategory, int itemExpiration,
+                boolean itemExpires, String itemName, int itemPrice, int itemQuantity
+            )
+    {
+        Table currentTable = getTable(storeID);
+        Item newItem = new Item()
+                .withPrimaryKey("ItemID", itemID)
+                .withString("Category", itemCategory)
+                .withInt("Expiration", itemExpiration)
+                .withBoolean("Expires", itemExpires)
+                .withString("Name", itemName)
+                .withInt("Price", itemPrice)
+                .withInt("Quantity", itemQuantity);
+        currentTable.putItem(newItem);
     }
 }
