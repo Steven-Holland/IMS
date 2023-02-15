7a3962aa7c30ae7ea2e62d035071eeef74e21efe
ethan <knot1657@kettering.edu>
2023-02-15 16:24:55 -0500

    test

--- src/main/java/cs351/group5/App.java
+++ src/main/java/cs351/group5/App.java
@@ -1,13 +1,15 @@
 package cs351.group5;
+import com.amazonaws.services.dynamodbv2.*;
 
 /**
  * Hello world!
- *
+ *origin/Ethan-Test
  */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
     }
 }
