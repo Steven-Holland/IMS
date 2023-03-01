214592a8ca6c93ac9b17300f0ba4660ef5588608
Steven-Holland <stevenmholland1@gmail.com>
2023-03-01 12:09:18 -0500

    Fixed expiration date field

--- src/main/java/com/example/ims/ItemController.java
+++ src/main/java/com/example/ims/ItemController.java
@@ -2,14 +2,12 @@ package com.example.ims;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.net.URL;
+import java.time.format.DateTimeParseException;
 import java.util.ResourceBundle;
 
 public class ItemController implements Initializable {
@@ -34,12 +32,23 @@ public class ItemController implements Initializable {
     private Button btn_add;
     @FXML
     private Label text_warning;
+    @FXML
+    private Label label_category;
+    @FXML
+    private Label label_expiration;
 
     SharedStorage storage = SharedStorage.getInstance();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         category_selector.getItems().addAll(categories);
+        category_selector.setOnAction(this::categoryMonitor);
+        expiration_date.getEditor().setDisable(true);
+
+        expiration_date.setVisible(false);
+        label_expiration.setVisible(false);
+        category_selector.setLayoutX(115);
+        label_category.setLayoutX(150);
     }
 
     @FXML
@@ -47,7 +56,7 @@ public class ItemController implements Initializable {
         if(checkFields()) {
             // add item to the database
             /*App.newItem(storage.getStore(), Integer.parseInt(text_id.getText()), category_selector.getValue(),
-                        expiration_date.getValue().toString(), true, text_name.getText(),
+                        expiration_date.getEditor().getText(), true, text_name.getText(),
                         Integer.parseInt(text_price.getText()), Integer.parseInt(text_quantity.getText()));*/
 
             Stage stage = (Stage) btn_add.getScene().getWindow();
@@ -55,6 +64,7 @@ public class ItemController implements Initializable {
         }
     }
 
+    // Verify all fields are filled with valid data
     private boolean checkFields(){
         if(text_id.getText().isEmpty() || text_name.getText().isEmpty() ||
                 text_price.getText().isEmpty() || text_quantity.getText().isEmpty()){
@@ -66,6 +76,34 @@ public class ItemController implements Initializable {
             return false;
         }
 
+        // Check if date entered is valid
+        try{
+            if(category_selector.getValue().equals("Consumables") &&
+                    expiration_date.getValue() == null){
+                text_warning.setText("Please enter a valid expiration date");
+                return false;
+            }
+            expiration_date.getConverter().fromString(expiration_date.getEditor().getText());
+        }catch(DateTimeParseException e){
+            text_warning.setText("Please enter a valid expiration date");
+            return false;
+        }
         return true;
+    }
+
+    // Reveal expiration date field if item is consumable
+    @FXML
+    private void categoryMonitor(ActionEvent event){
+        if(category_selector.getValue().equals("Consumables")){
+            category_selector.setLayoutX(42);
+            label_category.setLayoutX(78);
+            expiration_date.setVisible(true);
+            label_expiration.setVisible(true);
+        }else{
+            expiration_date.setVisible(false);
+            label_expiration.setVisible(false);
+            category_selector.setLayoutX(115);
+            label_category.setLayoutX(150);
+        }
     }
 }
